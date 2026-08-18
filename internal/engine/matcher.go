dd84a8a93f23f5326f3056245b0d44c645e4e81b
Harsh Kapse <harshkapse.dev@gmail.com>
2026-08-18 14:13:02 +0530

    perf(engine): short-circuit declaration matching when all declarations are mapped

--- internal/engine/matcher.go
+++ internal/engine/matcher.go
@@ -379,24 +379,45 @@ type decKey struct {
 
 func matchDeclarations(t1Root, t2Root *treesitter.ASTNode, m *Mapping) {
 	t1Decs := findDeclarations(t1Root)
+	if len(t1Decs) == 0 {
+		return
+	}
 	t2Decs := findDeclarations(t2Root)
+	if len(t2Decs) == 0 {
+		return
+	}
+
+	var u1 []*treesitter.ASTNode
+	for _, d1 := range t1Decs {
+		if !m.Has(d1) {
+			u1 = append(u1, d1)
+		}
+	}
+	if len(u1) == 0 {
+		return
+	}
+
+	var u2 []*treesitter.ASTNode
+	for _, d2 := range t2Decs {
+		if !m.HasDst(d2) {
+			u2 = append(u2, d2)
+		}
+	}
+	if len(u2) == 0 {
+		return
+	}
+
 	rules := rulesFor(t1Root)
 
-	t1Map := make(map[decKey][]*treesitter.ASTNode, len(t1Decs))
-	for _, d1 := range t1Decs {
-		if m.Has(d1) {
-			continue
-		}
+	t1Map := make(map[decKey][]*treesitter.ASTNode, len(u1))
+	for _, d1 := range u1 {
 		key := decKey{name: getDeclarationName(d1), rec: getReceiverTypeName(d1)}
 		if key.name != "" {
 			t1Map[key] = append(t1Map[key], d1)
 		}
 	}
-	t2Map := make(map[decKey][]*treesitter.ASTNode, len(t2Decs))
-	for _, d2 := range t2Decs {
-		if m.HasDst(d2) {
-			continue
-		}
+	t2Map := make(map[decKey][]*treesitter.ASTNode, len(u2))
+	for _, d2 := range u2 {
 		key := decKey{name: getDeclarationName(d2), rec: getReceiverTypeName(d2)}
 		if key.name != "" {
 			t2Map[key] = append(t2Map[key], d2)
